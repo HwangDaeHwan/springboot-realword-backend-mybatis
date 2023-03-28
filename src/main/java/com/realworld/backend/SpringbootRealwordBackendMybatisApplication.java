ac860e85feda1e8a2a21b833991c809336235ad0
HwangDaeHwan <nova0029@naver.com>
2023-03-28 16:21:33 +0900

    init

---
+++ src/main/java/com/realworld/backend/SpringbootRealwordBackendMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.realworld.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRealwordBackendMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRealwordBackendMybatisApplication.class, args);
+	}
+
+}
